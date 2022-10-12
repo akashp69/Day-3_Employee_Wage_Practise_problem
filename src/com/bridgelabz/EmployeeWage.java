d3cc452393df91bc96ca57e0c55db4bd1258bcd4
akashp69 <akashpakhare499@gmail.com>
2022-10-12 15:20:35 +0530

    Added:Employee Attendanace

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -3,5 +3,11 @@ package com.bridgelabz;
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage");
+        int empCheck = (int)Math.floor(Math.random()*10)%2;// 0 , 1
+        if(empCheck == 1)
+            System.out.println("Employee Present");
+        else
+            System.out.println("Employee Absent");
+
     }
 }
