15830ab760ca7e985d1c963967e67e63953100b9
akashp69 <akashpakhare499@gmail.com>
2022-10-14 02:45:17 +0530

    Added:Program For Calculating Monthly Per Day wage

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -1,32 +1,37 @@
 package com.bridgelabz;
 
 public class EmployeeWage {
-    static final int WAGE_PER_HOUR=20;
-    static final int FULL_TIME_HOUR=8;
+    static final int WAGE_PER_HOUR = 20;
+    static final int FULL_TIME_HOUR = 8;
     static final int PART_TIME_HOUR = 8;
     static final int IS_FULL_TIME = 2;
     static final int IS_PART_TIME = 1;
+    static final int WORKING_DAYS_PER_MONTH = 20;
+
     public static void main(String[] args) {
 
         System.out.println("Welcome to Employee Wage");
-        int dailyWage = 0;
-        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+        int day = 1;
+        while (day <= WORKING_DAYS_PER_MONTH) {
+            int dailyWage = 0;
+            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 
-        switch  (empCheck){
-            case IS_FULL_TIME:
-                System.out.println("Employee FULL TIME");
-                dailyWage = WAGE_PER_HOUR * FULL_TIME_HOUR;
-                break;
-            case IS_PART_TIME:
-                System.out.println("Employee PART TIME");
-                dailyWage = WAGE_PER_HOUR * PART_TIME_HOUR;
-                break;
-            default:
-                System.out.println("Employee Absent");
+            switch (empCheck) {
+                case IS_FULL_TIME:
+                    System.out.println("Employee FULL TIME");
+                    dailyWage = WAGE_PER_HOUR * FULL_TIME_HOUR;
+                    break;
+                case IS_PART_TIME:
+                    System.out.println("Employee PART TIME");
+                    dailyWage = WAGE_PER_HOUR * PART_TIME_HOUR;
+                    break;
+                default:
+                    System.out.println("Employee Absent");
 
+            }
+
+            System.out.println("Daily Wage => " + dailyWage);
+            day++;
         }
-
-
-        System.out.println("Daily Wage => " + dailyWage);
     }
 }
